38577866bf11d1f8e10dc5cd1e95dfd2a53ecf47
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-28 18:29:00 +0100

    Jo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(collection, search) {
+  return collection.filter(function(el) {
+    return el.toLowerCase() === search.toLowerCase();
+  })
+}
+
+
+function fuzzyMatch(collection, search) {
+  return collection.filter(function(el) {
+    return el.startsWith(search);
+  })
+}
+
+function matchName(collection, search) {
+  return collection.filter(function(el) {
+    return el.name === search;
+  })
+}
