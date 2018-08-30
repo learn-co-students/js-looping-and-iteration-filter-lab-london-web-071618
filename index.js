eff14065587f066b6596f0549c11c929b157adeb
github username <github email address>
2018-08-30 10:32:39 +0100

    Done.

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ function findMatching(driversArray, name) {
   })
 }
 
+// const findMatching = (driversArray, name) => {
+//   return driversArray.filter( x => x.toLowerCase() === name.toLowerCase())
+// }
+
 function fuzzyMatch(driversArray, name) {
   return driversArray.filter(function(driversName) {
     return driversName.slice(0,2).includes(name)
   })
 }
 
+// const fuzzyMatch = (driversArray, partialName) => {
+//   return driversArray.filter(name => name.slice(0,2) === partialName )
+// }
+
 function matchName(driversArray, name) {
   return driversArray.filter(function(nameMatch) {
     return nameMatch["name"] === name
   })
 }
+// 
+// const matchName = (driversArray, name) => {
+//   return driversArray.filter( driver => driver.name === name)
+// }
